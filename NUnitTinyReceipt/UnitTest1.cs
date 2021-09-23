cdbf5de755e322a01cd573517498dab7c2548e3b
nmtyping <3183593420@qq.com>
2021-09-24 00:34:58 +0800

    change unit test

--- NUnitTinyReceipt/UnitTest1.cs
+++ NUnitTinyReceipt/UnitTest1.cs
@@ -13,6 +13,7 @@ namespace NUnitTinyReceipt
         [Test]
         public void TaxRoundingTest()
         {
+            Assert.AreEqual(ReceiptService.RoundingTax(0.002M), 0.00M);
             Assert.AreEqual(ReceiptService.RoundingTax(0.035M), 0.05M);
             Assert.AreEqual(ReceiptService.RoundingTax(0.045M), 0.05M);
             Assert.AreEqual(ReceiptService.RoundingTax(0.056M), 0.10M);
